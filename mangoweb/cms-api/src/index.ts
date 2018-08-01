23f038561105261afef3f7f5a93d21858fc1bca0
Jan Tvrdik <jan.tvrdik@gmail.com>
2018-08-01 09:05:09 +0200

    integrate tenant-api to cms-api

--- mangoweb/cms-api/src/index.ts
+++ mangoweb/cms-api/src/index.ts
@@ -1,41 +1,69 @@
-import * as dotenv from "dotenv"
-import * as fs from "fs"
-import { printSchema } from "graphql"
-import { GraphQLServer } from "graphql-yoga"
-import * as knex from "knex"
-import model from "./projects/blog/src/model"
-import { getSqlSchema, GraphQlSchemaBuilder } from 'cms-api'
+import * as knex from 'knex'
+import blogModel from './projects/blog/src/model'
+import {CompositionRoot, Env, getSqlSchema, Project} from 'cms-api'
 
-dotenv.config()
+(async () => {
+  const env = Env.fromUnsafe(process.env)
 
-const builder = new GraphQlSchemaBuilder(model)
-const graphQLSchema = builder.build()
+  const projects: Array<Project> = [
+    {
+      uuid: '1635399a-de76-4101-bd14-82d67e9d2574',
+      slug: 'blog',
+      name: 'Blog',
 
-const sql = getSqlSchema(model)
-fs.writeFile(__dirname + "/../generated/schema.sql", sql, error => console.error(error))
+      stages: [
+        {
+          uuid: '01c8f908-6f48-4ef3-9751-9989931b42eb',
+          slug: 'prod',
+          name: 'Production',
+          schema: blogModel,
+        },
+      ],
 
-const fileData = printSchema(graphQLSchema as any, {commentDescriptions: true})
+      dbCredentials: {
+        host: env.DB_HOST,
+        port: Number.parseInt(env.DB_PORT),
+        user: env.DB_USER,
+        password: env.DB_PASSWORD,
+        database: env.DB_DATABASE,
+      },
+    },
+  ]
 
-fs.writeFile(__dirname + "/../generated/schema.graphql", fileData, error => console.error(error))
+  projects.forEach(project => {
+    project.stages.forEach(stage => {
+      const pgSchemaName = 'public' // TODO: should depend on stage
+      const sql = getSqlSchema(stage.schema)
 
-const connection = knex({
-  debug: true,
-  client: "pg",
-  connection: {
-    host: process.env.DB_HOST as string,
-    port: process.env.DB_PORT as string,
-    user: process.env.DB_USER as string,
-    password: process.env.DB_PASSWORD as string,
-    database: process.env.DB_DATABASE as string,
-  }
-})
+      const db = knex({
+        debug: false,
+        client: 'pg',
+        connection: {
+          host: project.dbCredentials.host,
+          port: project.dbCredentials.port,
+          user: project.dbCredentials.user,
+          password: project.dbCredentials.password,
+          database: project.dbCredentials.database,
+        },
+      })
 
-// const result = generate
-const server = new GraphQLServer({
-  schema: graphQLSchema as any,
-  context: {
-    db: connection,
-  },
+      console.log(`Rebuilding schema ${project.dbCredentials.database}.${pgSchemaName}`)
+      db.transaction(async () => {
+        await db.raw(`DROP SCHEMA IF EXISTS ${pgSchemaName} CASCADE`)
+        await db.raw(`CREATE SCHEMA ${pgSchemaName}`)
+        await db.raw(sql)
+      })
+    })
+  })
 
-})
-server.start({port: process.env.SERVER_PORT}, options => console.log(`Server is running on localhost:${options.port}`))
+  const compositionRoot = new CompositionRoot()
+  const httpServer = compositionRoot.composeServer(env, projects)
+  httpServer.listen(Number.parseInt(env.SERVER_PORT), () => {
+    console.log(`Tenant API running on http://localhost:${env.SERVER_PORT}/tenant`)
+    projects.forEach(project => {
+      project.stages.forEach(stage => {
+        console.log(`Content API for project ${project.slug} and stage ${stage.slug} running on http://localhost:${env.SERVER_PORT}/content/${project.slug}/${stage.slug}`)
+      })
+    })
+  })
+})()
