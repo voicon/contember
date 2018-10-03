3d14a58163f9e8c4c1ebcf85803cd9d1fd71c93f
David Matejka <matej21@matej21.cz>
2018-10-03 13:38:01 +0200

    cs fix

--- mangoweb/cms-api/src/index.ts
+++ mangoweb/cms-api/src/index.ts
@@ -2,10 +2,9 @@ import blogModel from './projects/blog/src/model'
 import { CompositionRoot, parseConfig, Project } from 'cms-api'
 import * as fs from 'fs'
 import * as path from 'path'
-import { promisify } from 'util';
+import { promisify } from 'util'
 import { Schema } from 'cms-common'
-
-(async () => {
+;(async () => {
 	const fsRead = promisify(fs.readFile)
 
 	const file = await fsRead(path.dirname(__filename) + '/../../src/config/config.yaml', { encoding: 'utf8' })
@@ -15,14 +14,13 @@ import { Schema } from 'cms-common'
 		blog: blogModel
 	}
 
-	const projects: Array<Project> = config.projects.map(project => (
-		{
-			...project,
-			stages: project.stages.map(stage => ({
-				...stage,
-				schema: schemaMap[project.slug],
-			}))
+	const projects: Array<Project> = config.projects.map(project => ({
+		...project,
+		stages: project.stages.map(stage => ({
+			...stage,
+			schema: schemaMap[project.slug]
 		}))
+	}))
 
 	const compositionRoot = new CompositionRoot()
 	const httpServer = compositionRoot.composeServer(config.tenant.db, projects)
@@ -31,14 +29,11 @@ import { Schema } from 'cms-common'
 		projects.forEach(project => {
 			project.stages.forEach(stage => {
 				const url = `http://localhost:${config.server.port}/content/${project.slug}/${stage.slug}`
-				console.log(
-					`Content API for project ${project.slug} and stage ${stage.slug} running on ${url}`
-				)
+				console.log(`Content API for project ${project.slug} and stage ${stage.slug} running on ${url}`)
 			})
 		})
 	})
-})()
-	.catch(e => {
-		console.log(e)
-		process.exit(1)
-	})
+})().catch(e => {
+	console.log(e)
+	process.exit(1)
+})
