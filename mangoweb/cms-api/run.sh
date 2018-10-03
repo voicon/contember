f323b5b81740df3dcaecdb0957e6d724b73cfad4
David Matejka <matej21@matej21.cz>
2018-10-03 14:39:36 +0200

    fix run.sh

--- mangoweb/cms-api/run.sh
+++ mangoweb/cms-api/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-node ./node_modules/.bin/migrations-continue src/config/config.yaml
-node ./node_modules/.bin/init src/config/config.yaml src/projects
+node ./node_modules/cms-api/dist/src/bin/migrations-continue.js src/config/config.yaml
+node ./node_modules/cms-api/dist/src/bin/init.js src/config/config.yaml src/projects
 node ./dist/src/index.js
