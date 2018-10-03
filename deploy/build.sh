ac4fce253705d11b9c172a4914e917b5b97f4ff0
David Matejka <matej21@matej21.cz>
2018-10-03 14:43:43 +0200

    fix ci config

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ECR="831119889470.dkr.ecr.eu-central-1.amazonaws.com"
-REPO="mangoweb/app/cms-admin"
 
 ecr-login
 
+REPO="mangoweb/app/cms-admin"
 docker build -t "$ECR/$REPO:$VERSION" ./mangoweb/admin
 docker push "$ECR/$REPO:$VERSION"
 
