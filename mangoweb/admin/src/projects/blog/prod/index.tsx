1d4fdaf34e799c87c9724c86755d3afa5ae69164
Jirka Vebr <jirkavebr@gmail.com>
2018-10-03 15:56:03 +0200

    Refactoring: abolish the <Entity /> component

--- mangoweb/admin/src/projects/blog/prod/index.tsx
+++ mangoweb/admin/src/projects/blog/prod/index.tsx
@@ -34,37 +34,27 @@ export default (
 			<RichTextField name="content" label="Content" allowLineBreaks={false} />
 			<h2>Featured links</h2>
 			<Repeater field="featuredLinks">
-				<Entity name="FeaturedLink">
-					<TextField name="title" label="Title" />
-					<TextField name="url" label="URL" />
-					<TextField name="color" label="Color" />
-					<hr />
-				</Entity>
+				<TextField name="title" label="Title" />
+				<TextField name="url" label="URL" />
+				<TextField name="color" label="Color" />
+				<hr />
 			</Repeater>
 		</EditPage>
 
 		<Page<{ edit_post2: { id: string } }> name="edit_post2">
 			{({ id }) => (
 				<Layout>
-					<SingleEntityDataProvider where={{ id }}>
-						<Entity name="Post">
-							<TextField name="publishedAt" label="Time" />
-							<SelectField name="author" label="Author" entityName="Author" optionFieldName="name" />
-							<ToMany field="categories">
-								<Entity name="Category">
-									<ToMany field="locales">
-										<Entity name="CategoryLocale" where={{ locale: { eq: new GraphQlBuilder.Literal('cs') } }}>
-											<TextField name="name" label="Name" />
-										</Entity>
-									</ToMany>
-								</Entity>
+					<SingleEntityDataProvider name="Post" where={{ id }}>
+						<TextField name="publishedAt" label="Time" />
+						<SelectField name="author" label="Author" entityName="Author" optionFieldName="name" />
+						<ToMany field="categories">
+							<ToMany field="locales" where={{ locale: { eq: new GraphQlBuilder.Literal('cs') } }}>
+								<TextField name="name" label="Name" />
 							</ToMany>
-							<ToMany field="locales">
-								<Entity name="PostLocale">
-									<TextField name="title" label="Title" />
-								</Entity>
-							</ToMany>
-						</Entity>
+						</ToMany>
+						<ToMany field="locales">
+							<TextField name="title" label="Title" />
+						</ToMany>
 					</SingleEntityDataProvider>
 				</Layout>
 			)}
@@ -74,14 +64,10 @@ export default (
 			{() => (
 				<Layout>
 					<h1>All posts</h1>
-					<EntityListDataProvider>
-						<Entity name="Post">
-							<ToMany field="locales">
-								<Entity name="PostLocale" where={{ locale: { eq: new GraphQlBuilder.Literal('cs') } }}>
-									<TextField name="title" label="Title" />
-								</Entity>
-							</ToMany>
-						</Entity>
+					<EntityListDataProvider name="Post">
+						<ToMany field="locales" where={{ locale: { eq: new GraphQlBuilder.Literal('cs') } }}>
+							<TextField name="title" label="Title" />
+						</ToMany>
 					</EntityListDataProvider>
 				</Layout>
 			)}
