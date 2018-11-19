ebf1e110dcf0392179d92f163a0503073e6d32c9
Jirka Vebr <jirkavebr@gmail.com>
2018-11-19 15:42:53 +0100

    Migrate contact locations

---
+++ mangoweb/cms-api/src/projects/mangoweb/migrations/2018-11-19-154155-contactLocations2.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "contact_location_locale"
+  ADD "description" text;
+ALTER TABLE "contact_location_locale"
+  ADD "additional_info" text;
+ALTER TABLE "contact_location_locale"
+  DROP "additional_text";
