e4d07d50d289c3e3f7b6833c2c706ab8ef44fd96
Jirka Vebr <jirkavebr@gmail.com>
2019-01-03 15:46:10 +0100

    Simpler custom data binding components

--- mangoweb/admin/src/projects/mangoweb/preview/components/LocaleSideDimension.tsx
+++ mangoweb/admin/src/projects/mangoweb/preview/components/LocaleSideDimension.tsx
@@ -2,34 +2,26 @@ import { Component, SideDimensions, Variable } from 'cms-admin'
 import * as React from 'react'
 import { LangDimension } from '../dimensions'
 
-interface LangSideDimensionProps {
-	children: React.ReactNode
-}
+interface LangSideDimensionProps {}
 
-export const LocaleSideDimension = Component(
-	class extends React.Component<LangSideDimensionProps> {
-		public static render(props: LangSideDimensionProps) {
-			return (
-				<SideDimensions
-					dimension="lang"
-					variables={currentLang => {
-						return {
-							locale: `locales(locale=${currentLang})`,
-							flag: {
-								en: '🇬🇧',
-								cs: '🇨🇿'
-							}[currentLang as LangDimension],
-							labelMiddleware: label => (
-								<>
-									<Variable name="flag" /> {label}
-								</>
-							)
-						}
-					}}
-				>
-					{props.children}
-				</SideDimensions>
-			)
-		}
-	}
-)
+export const LocaleSideDimension = Component<LangSideDimensionProps>(props => (
+	<SideDimensions
+		dimension="lang"
+		variables={currentLang => {
+			return {
+				locale: `locales(locale=${currentLang})`,
+				flag: {
+					en: '🇬🇧',
+					cs: '🇨🇿'
+				}[currentLang as LangDimension],
+				labelMiddleware: label => (
+					<>
+						<Variable name="flag" /> {label}
+					</>
+				)
+			}
+		}}
+	>
+		{props.children}
+	</SideDimensions>
+))
