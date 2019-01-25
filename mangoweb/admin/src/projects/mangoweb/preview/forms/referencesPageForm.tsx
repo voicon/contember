0f0d4a138ea38b1c593840067ac13b83b3d6d07b
Jirka Vebr <jirkavebr@gmail.com>
2019-01-25 10:57:01 +0100

    References

--- mangoweb/admin/src/projects/mangoweb/preview/forms/referencesPageForm.tsx
+++ mangoweb/admin/src/projects/mangoweb/preview/forms/referencesPageForm.tsx
@@ -1,7 +1,7 @@
 import { H2 } from '@blueprintjs/core'
-import { SortableRepeater, TextField } from 'cms-admin'
+import { TextField } from 'cms-admin'
 import * as React from 'react'
-import { ImageField, LocaleSideDimension } from '../components'
+import { LocaleSideDimension } from '../components'
 
 export const referencesPageForm = (
 	<>
@@ -9,16 +9,6 @@ export const referencesPageForm = (
 		<LocaleSideDimension>
 			<TextField name="$locale.titleShort" label="Abbreviated page title" />
 			<TextField name="$locale.titleFull" label="Full page title" />
-		</LocaleSideDimension>
-
-		<H2>References</H2>
-		<LocaleSideDimension>
-			<SortableRepeater sortBy="order" field="$locale.references">
-				<TextField name="title" label="Title" />
-				<ImageField name="image" label="Reference image" />
-				<TextField name="url" label="Reference link url" />
-				<TextField name="urlLabel" label="Reference link text" />
-			</SortableRepeater>
 		</LocaleSideDimension>
 	</>
 )
