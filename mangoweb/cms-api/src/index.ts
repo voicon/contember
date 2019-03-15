5877c93a5bf879cede2f31d73eda068ddf0e9636
David Matejka <matej21@matej21.cz>
2019-03-15 18:19:07 +0100

    rework migrations WIP

---
+++ mangoweb/cms-api/src/index.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { CompositionRoot, readConfig } from 'cms-api'
+import * as path from 'path'
+import Quanti from './projects/quanti/src/model'
+import Mangoweb from './projects/mangoweb/src/model'
+import Blog from './projects/blog/src/model'
+
+
+(async () => {
+	const configFile = path.join(__dirname, '../../src/config/config.yaml')
+	const config = await readConfig(configFile)
+	const projectsDirectory = path.join(__dirname, '../../src/projects')
+
+	const container = new CompositionRoot().createMasterContainer(config, projectsDirectory, {
+			quanti: Quanti,
+			mangoweb: Mangoweb,
+			blog: Blog,
+		}
+	)
+
+	container.cli.run(process.argv)
+
+})().catch(e => {
+	console.log(e)
+	process.exit(1)
+})
