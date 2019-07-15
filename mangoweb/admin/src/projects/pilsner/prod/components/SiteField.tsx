6d09396884d01169d19651a765932fc43b14df13
Jirka Vebr <jirkavebr@gmail.com>
2019-07-15 18:19:40 +0200

    SiteField: use Component

--- mangoweb/admin/src/projects/pilsner/prod/components/SiteField.tsx
+++ mangoweb/admin/src/projects/pilsner/prod/components/SiteField.tsx
@@ -1,4 +1,7 @@
-import { SelectField } from 'cms-admin'
+import { Component, SelectField } from 'cms-admin'
 import * as React from 'react'
 
-export const SiteField = () => <SelectField options={'Site[slug=$site].name'} name={'site'} label={'Site'} />
+export const SiteField = Component(
+	() => <SelectField options="Site[slug=$site].name" name={'site'} label={'Site'} />,
+	'SiteField'
+)
