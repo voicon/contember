db52da6a5f16f309f54a8f2554389445736e6307
David Matejka <matej21@matej21.cz>
2019-07-15 09:56:26 +0200

    cs fix

--- mangoweb/admin/src/projects/pilsner/prod/pages/DashboardPage.tsx
+++ mangoweb/admin/src/projects/pilsner/prod/pages/DashboardPage.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react'
 import { GenericPage, readEventStream, readLines } from 'cms-admin'
 
-
 export const DashboardPage = (
 	<GenericPage pageName="dashboard">
-		<h1>
-			PilsnerUrquell.com admin
-		</h1>
+		<h1>PilsnerUrquell.com admin</h1>
 	</GenericPage>
 )
