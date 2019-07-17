8c49120be75d2e56206b186d27bf494b7fc028cd
Jirka Vebr <jirkavebr@gmail.com>
2019-07-17 13:41:31 +0200

    Add support for explicit connections

--- mangoweb/admin/src/projects/pilsner/prod/components/SiteField.tsx
+++ mangoweb/admin/src/projects/pilsner/prod/components/SiteField.tsx
@@ -1,7 +1,6 @@
-import { Component, SelectField } from 'cms-admin'
+import { Component, Connection } from 'cms-admin'
 import * as React from 'react'
 
-export const SiteField = Component(
-	() => <SelectField options="Site[slug=$site].name" name={'site'} label={'Site'} />,
-	'SiteField'
-)
+export const SiteField = Component(() => null, 'SiteField', (props, environment) => (
+	<Connection field="site" to={{ slug: environment.getDimensionOrElse('site', ['en'])[0] }} />
+))
