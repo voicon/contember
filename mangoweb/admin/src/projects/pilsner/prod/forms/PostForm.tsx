2d866d253edec51abd03dac08a3940847e6b5753
David Matejka <matej21@matej21.cz>
2019-07-17 16:38:54 +0200

    pilsner forms: add env in tags form, add categories checkboxlist

--- mangoweb/admin/src/projects/pilsner/prod/forms/PostForm.tsx
+++ mangoweb/admin/src/projects/pilsner/prod/forms/PostForm.tsx
@@ -1,23 +1,27 @@
 import * as React from 'react'
-import { CheckboxList, Component, TextAreaField, TextField } from 'cms-admin'
+import { CheckboxList, Component, Environment, EnvironmentContext, TextAreaField, TextField } from 'cms-admin'
 import { ImageField } from '../components'
 import { GenericContentForm, LinkForm, SeoForm } from './'
+import { getSite } from '../utils/environment'
+
+const PostFormInner = (env: Environment) => (
+	<>
+		<TextField name="title" large={true} label={'Title'} />
+		<LinkForm />
+		<ImageField name={'headerImage'} label={'Header image'} />
+		<ImageField name={'listingImage'} label={'Listing image'} />
+		<TextField name="imageDescription" label="Image description" />
+
+		<TextAreaField name="perex" label="Perex" />
+		<GenericContentForm />
+		<CheckboxList name="tags" options={`Tag[site.slug = '${getSite(env)}'].name`} label="Tags" />
+		<CheckboxList name="categories" options={`Category[site.slug = '${getSite(env)}'].name`} label="Categories" />
+		<SeoForm />
+	</>
+)
 
 export const PostForm = Component(
-	() => (
-		<>
-			<TextField name="title" large={true} label={'Title'} />
-			<LinkForm />
-			<ImageField name={'headerImage'} label={'Header image'} />
-			<ImageField name={'listingImage'} label={'Listing image'} />
-			<TextField name="imageDescription" label="Image description" />
-
-			<TextAreaField name="perex" label="Perex" />
-			<GenericContentForm />
-			<CheckboxList name="tags" options="Tag[site.slug = 'en'].name" label="Tags" />
-
-			<SeoForm />
-		</>
-	),
-	'PostForm'
+	() => <EnvironmentContext.Consumer>{PostFormInner}</EnvironmentContext.Consumer>,
+	'PostForm',
+	(props, env) => PostFormInner(env)
 )
--- mangoweb/admin/src/projects/pilsner/prod/forms/TapsterForm.tsx
+++ mangoweb/admin/src/projects/pilsner/prod/forms/TapsterForm.tsx
@@ -1,31 +1,43 @@
 import * as React from 'react'
-import { CheckboxList, Component, MultipleSelectField, SelectField, TextAreaField, TextField } from 'cms-admin'
+import {
+	CheckboxList,
+	Component,
+	Environment,
+	EnvironmentContext,
+	SelectField,
+	TextAreaField,
+	TextField
+} from 'cms-admin'
 import { ImageField } from '../components'
 import { AttributesForm, GenericContentForm, LinkForm, SeoForm } from './'
+import { getSite } from '../utils/environment'
 
 interface TapsterFormProps {
 	withLocation: boolean
 }
 
+const TapsterFormInner = (props: TapsterFormProps, env: Environment) => (
+	<>
+		<ImageField name={'headerImage'} label={'Header image'} />
+		<ImageField name={'listingImage'} label={'Listing image'} />
+		<TextField name="imageDescription" label="Image description" />
+
+		<TextField name="name" label="Name" />
+		<TextField name="subtitle" label="Subtitle" />
+		<TextField name="locationText" label="Location" />
+		{props.withLocation && <SelectField name={'location'} label={'Location'} options={'Location.name'} />}
+		<TextAreaField name="perex" label="Perex" />
+		<AttributesForm field={'attributeSet'} />
+		<GenericContentForm />
+		<CheckboxList name="tags" options={`Tag[site.slug = '${getSite(env)}'].name`} label="Tags" />
+
+		<LinkForm />
+		<SeoForm />
+	</>
+)
+
 export const TapsterForm = Component<TapsterFormProps>(
-	props => (
-		<>
-			<ImageField name={'headerImage'} label={'Header image'} />
-			<ImageField name={'listingImage'} label={'Listing image'} />
-			<TextField name="imageDescription" label="Image description" />
-
-			<TextField name="name" label="Name" />
-			<TextField name="subtitle" label="Subtitle" />
-			<TextField name="locationText" label="Location" />
-			{props.withLocation && <SelectField name={'location'} label={'Location'} options={'Location.name'} />}
-			<TextAreaField name="perex" label="Perex" />
-			<AttributesForm field={'attributeSet'} />
-			<GenericContentForm />
-			<CheckboxList name="tags" options="Tag[site.slug = 'en'].name" label="Tags" />
-
-			<LinkForm />
-			<SeoForm />
-		</>
-	),
-	'TapsterForm'
+	props => <EnvironmentContext.Consumer>{env => TapsterFormInner(props, env)}</EnvironmentContext.Consumer>,
+	'TapsterForm',
+	TapsterFormInner
 )
