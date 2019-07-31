ab52d7911ad64f68d28a6ff00023da3dfb8aa461
David Matejka <matej21@matej21.cz>
2019-07-31 18:15:25 +0200

    rework project structure

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,10 +5,19 @@ ECR="831119889470.dkr.ecr.eu-central-1.amazonaws.com"
 
 ecr-login
 
-REPO="mangoweb/app/cms-admin"
-docker build -t "$ECR/$REPO:$VERSION" ./mangoweb/admin
-docker push "$ECR/$REPO:$VERSION"
+APP=$1
+if [$APP != ""]; then
+  PATH = "$APP/"
+else
+  PATH = ""
+fi
 
-REPO="mangoweb/app/cms-api"
+REPO="mangoweb/app/${APP}cms-admin"
+docker build -t "$ECR/$REPO:$VERSION" ./mangoweb/admin
+docker tag "$ECR/$REPO:$VERSION" "$ECR/$REPO:latest"
+docker push "$ECR/$REPO"
+
+REPO="mangoweb/app/${APP}cms-api"
 docker build -t "$ECR/$REPO:$VERSION" ./mangoweb/cms-api
-docker push "$ECR/$REPO:$VERSION"
+docker tag "$ECR/$REPO:$VERSION" "$ECR/$REPO:latest"
+docker push "$ECR/$REPO"
---
+++ projects/quanti/admin/src/prod/LocaleSideDimension.tsx
@@ -0,0 +1,29 @@
+import { Component, SideDimensions, ToOne, Variable } from 'cms-admin'
+import * as React from 'react'
+
+type LangDimension = 'cs' | 'en'
+
+
+interface LangSideDimensionProps {}
+
+export const LocaleSideDimension = Component<LangSideDimensionProps>(props => (
+	<SideDimensions
+		dimension="locale"
+		variableName="currentLangSlug"
+		variables={currentLangSlug => {
+			return {
+				flag: {
+					en: '🇬🇧',
+					cs: '🇨🇿'
+				}[currentLangSlug as LangDimension],
+				labelMiddleware: label => (
+					<>
+						<Variable name="flag" /> {label}
+					</>
+				)
+			}
+		}}
+	>
+		<ToOne field="locales(locale.slug=$currentLangSlug)">{props.children}</ToOne>
+	</SideDimensions>
+))
