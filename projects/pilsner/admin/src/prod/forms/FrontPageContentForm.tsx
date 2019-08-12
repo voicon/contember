7215dd74e8ec1ee3c9e0b534976aa363aed70c5f
Jirka Vebr <jirkavebr@gmail.com>
2019-08-12 10:39:32 +0200

    Pilsner: utilize SortableBlockRepeaters

--- projects/pilsner/admin/src/prod/forms/FrontPageContentForm.tsx
+++ projects/pilsner/admin/src/prod/forms/FrontPageContentForm.tsx
@@ -1,125 +1,125 @@
-import { AlternativeFields, Component, Literal, SortableRepeater, TextAreaField, TextField, ToOne } from 'cms-admin'
+import { Component, Literal, SortableBlockRepeater, SortableRepeater, TextAreaField, TextField, ToOne } from 'cms-admin'
 import * as React from 'react'
 import { ImageField } from '../components'
 
 export const FrontPageContentForm = Component(
 	() => (
-		<SortableRepeater sortBy={'order'} field={'content.blocks'}>
-			<AlternativeFields
-				name="type"
-				alternatives={[
-					[
-						new Literal('frontTextBox'),
-						'Text box',
-						<>
-							<TextField name="title" label={'Title'} />
-							<TextField name="subtitle" label={'Subtitle'} />
-							<TextAreaField name="text" label={'Text'} />
-						</>,
-					],
-					[
-						new Literal('frontHalfImageLight'),
-						'"Light" image with text half split',
-						<>
-							<TextField name="title" label={'Title'} />
-							<TextAreaField name="text" label={'Text'} />
-							<ImageField name={'image'} label={'Image'} />
-							<TextField name="buttonCaption" label={'Button caption'} />
-							<TextField name="url" label={'Button url'} />
-						</>,
-					],
-					[
-						new Literal('frontHalfImageLeft'),
-						'Image with text half split left',
-						<>
-							<TextField name="title" label={'Title'} />
-							<TextAreaField name="text" label={'Text'} />
-							<ImageField name={'image'} label={'Image'} />
-						</>,
-					],
-					[
-						new Literal('frontHalfImageRight'),
-						'Image with text half split right',
-						<>
-							<TextField name="title" label={'Title'} />
-							<TextAreaField name="text" label={'Text'} />
-							<ImageField name={'image'} label={'Image'} />
-						</>,
-					],
-					[
-						new Literal('frontPhotoBoxLeft'),
-						'Photo box left',
-						<>
-							<TextField name="title" label={'Title'} />
-							<TextAreaField name="text" label={'Text'} />
-							<ImageField name={'image'} label={'Image'} />
-						</>,
-					],
-					[
-						new Literal('frontPhotoBoxRight'),
-						'Photo box right',
-						<>
-							<TextField name="title" label={'Title'} />
-							<TextAreaField name="text" label={'Text'} />
-							<ImageField name={'image'} label={'Image'} />
-						</>,
-					],
-					[
-						new Literal('frontLargeImage'),
-						'Large photo',
-						<>
-							<ImageField name={'image'} label={'Image'} />
-						</>,
-					],
-					[
-						new Literal('frontLargeImageWithTextBox'),
-						'Text and large photo',
-						<>
-							<TextField name="title" label={'Title'} />
-							<TextField name="subtitle" label={'Subtitle'} />
-							<TextAreaField name="text" label={'Text'} />
-							<ImageField name={'image'} label={'Image'} />
-						</>,
-					],
-					[
-						new Literal('frontPhoneGallery'),
-						'Phone gallery',
-						<>
-							<ToOne field={'gallery'}>
-								<SortableRepeater sortBy={'order'} field={'images'} removeType={'delete'}>
-									<ImageField name={'image'} label={'Image'} single={true} />
-								</SortableRepeater>
-							</ToOne>
-						</>,
-					],
-					[
-						new Literal('frontDesktopGallery'),
-						'Desktop gallery',
-						<>
-							<ToOne field={'gallery'}>
-								<SortableRepeater sortBy={'order'} field={'images'} removeType={'delete'}>
-									<ImageField name={'image'} label={'Image'} single={true} />
-								</SortableRepeater>
-							</ToOne>
-						</>,
-					],
-					[
-						new Literal('frontCarousel'),
-						'Carousel',
-						<>
-							<TextField name="title" label={'Title'} />
-							<TextAreaField name="text" label={'Text'} />
-							<ToOne field={'gallery'}>
-								<SortableRepeater sortBy={'order'} field={'images'} removeType={'delete'}>
-									<TextAreaField name="caption" label={'Caption'} />
-									<ImageField name={'image'} label={'Image'} />
-								</SortableRepeater>
-							</ToOne>
-						</>,
-					],
-				]}
-			/>
-		</SortableRepeater>
+		<SortableBlockRepeater
+			sortBy={'order'}
+			field={'content.blocks'}
+			discriminationField="type"
+			alternatives={[
+				[
+					new Literal('frontTextBox'),
+					'Text box',
+					<>
+						<TextField name="title" label={'Title'} />
+						<TextField name="subtitle" label={'Subtitle'} />
+						<TextAreaField name="text" label={'Text'} />
+					</>,
+				],
+				[
+					new Literal('frontHalfImageLight'),
+					'"Light" image with text half split',
+					<>
+						<TextField name="title" label={'Title'} />
+						<TextAreaField name="text" label={'Text'} />
+						<ImageField name={'image'} label={'Image'} />
+						<TextField name="buttonCaption" label={'Button caption'} />
+						<TextField name="url" label={'Button url'} />
+					</>,
+				],
+				[
+					new Literal('frontHalfImageLeft'),
+					'Image with text half split left',
+					<>
+						<TextField name="title" label={'Title'} />
+						<TextAreaField name="text" label={'Text'} />
+						<ImageField name={'image'} label={'Image'} />
+					</>,
+				],
+				[
+					new Literal('frontHalfImageRight'),
+					'Image with text half split right',
+					<>
+						<TextField name="title" label={'Title'} />
+						<TextAreaField name="text" label={'Text'} />
+						<ImageField name={'image'} label={'Image'} />
+					</>,
+				],
+				[
+					new Literal('frontPhotoBoxLeft'),
+					'Photo box left',
+					<>
+						<TextField name="title" label={'Title'} />
+						<TextAreaField name="text" label={'Text'} />
+						<ImageField name={'image'} label={'Image'} />
+					</>,
+				],
+				[
+					new Literal('frontPhotoBoxRight'),
+					'Photo box right',
+					<>
+						<TextField name="title" label={'Title'} />
+						<TextAreaField name="text" label={'Text'} />
+						<ImageField name={'image'} label={'Image'} />
+					</>,
+				],
+				[
+					new Literal('frontLargeImage'),
+					'Large photo',
+					<>
+						<ImageField name={'image'} label={'Image'} />
+					</>,
+				],
+				[
+					new Literal('frontLargeImageWithTextBox'),
+					'Text and large photo',
+					<>
+						<TextField name="title" label={'Title'} />
+						<TextField name="subtitle" label={'Subtitle'} />
+						<TextAreaField name="text" label={'Text'} />
+						<ImageField name={'image'} label={'Image'} />
+					</>,
+				],
+				[
+					new Literal('frontPhoneGallery'),
+					'Phone gallery',
+					<>
+						<ToOne field={'gallery'}>
+							<SortableRepeater sortBy={'order'} field={'images'} removeType={'delete'}>
+								<ImageField name={'image'} label={'Image'} single={true} />
+							</SortableRepeater>
+						</ToOne>
+					</>,
+				],
+				[
+					new Literal('frontDesktopGallery'),
+					'Desktop gallery',
+					<>
+						<ToOne field={'gallery'}>
+							<SortableRepeater sortBy={'order'} field={'images'} removeType={'delete'}>
+								<ImageField name={'image'} label={'Image'} single={true} />
+							</SortableRepeater>
+						</ToOne>
+					</>,
+				],
+				[
+					new Literal('frontCarousel'),
+					'Carousel',
+					<>
+						<TextField name="title" label={'Title'} />
+						<TextAreaField name="text" label={'Text'} />
+						<ToOne field={'gallery'}>
+							<SortableRepeater sortBy={'order'} field={'images'} removeType={'delete'}>
+								<TextAreaField name="caption" label={'Caption'} />
+								<ImageField name={'image'} label={'Image'} />
+							</SortableRepeater>
+						</ToOne>
+					</>,
+				],
+			]}
+		/>
 	),
 	'FrontPageContentForm',
 )
--- projects/pilsner/admin/src/prod/forms/GenericContentForm.tsx
+++ projects/pilsner/admin/src/prod/forms/GenericContentForm.tsx
@@ -1,11 +1,11 @@
 import {
-	AlternativeFields,
 	Block,
 	Component,
 	LineBreakBehavior,
 	Literal,
 	Mark,
 	RichTextField,
+	SortableBlockRepeater,
 	SortableRepeater,
 	TextAreaField,
 	TextField,
@@ -18,54 +18,54 @@ export const GenericContentForm = Component(
 	() => (
 		<>
 			<h2>Content</h2>
-			<SortableRepeater sortBy={'order'} field={'content.blocks'}>
-				<AlternativeFields
-					name="type"
-					alternatives={[
-						[
-							new Literal('contentTextSection'),
-							'Text section',
-							<>
-								<RichTextField
-									label="Text"
-									name="text"
-									lineBreakBehavior={LineBreakBehavior.SMART}
-									blocks={[
-										{ block: Block.PARAGRAPH, marks: [Mark.BOLD, Mark.LINK, Mark.ITALIC] },
-										{ block: Block.HEADING, marks: [] },
-									]}
-								/>
-							</>,
-						],
-						[
-							new Literal('contentImage'),
-							'Image',
-							<>
-								<ImageField name={'image'} label={'Image'} />
-							</>,
-						],
-						[
-							new Literal('contentHtml'),
-							'HTML',
-							<>
-								<TextAreaField name="text" label={'Text'} />
-							</>,
-						],
-						[
-							new Literal('contentGallery'),
-							'Desktop gallery',
-							<>
-								<ToOne field={'gallery'}>
-									<SortableRepeater sortBy={'order'} field={'images'} removeType={'delete'}>
-										<ImageField name={'image'} label={'Image'} />
-										<TextField name={'caption'} label={'Caption'} />
-									</SortableRepeater>
-								</ToOne>
-							</>,
-						],
-					]}
-				/>
-			</SortableRepeater>
+			<SortableBlockRepeater
+				sortBy="order"
+				field="content.blocks"
+				discriminationField="type"
+				alternatives={[
+					[
+						new Literal('contentTextSection'),
+						'Text section',
+						<>
+							<RichTextField
+								label="Text"
+								name="text"
+								lineBreakBehavior={LineBreakBehavior.SMART}
+								blocks={[
+									{ block: Block.PARAGRAPH, marks: [Mark.BOLD, Mark.LINK, Mark.ITALIC] },
+									{ block: Block.HEADING, marks: [] },
+								]}
+							/>
+						</>,
+					],
+					[
+						new Literal('contentImage'),
+						'Image',
+						<>
+							<ImageField name={'image'} label={'Image'} />
+						</>,
+					],
+					[
+						new Literal('contentHtml'),
+						'HTML',
+						<>
+							<TextAreaField name="text" label={'Text'} />
+						</>,
+					],
+					[
+						new Literal('contentGallery'),
+						'Desktop gallery',
+						<>
+							<ToOne field={'gallery'}>
+								<SortableRepeater sortBy={'order'} field={'images'} removeType={'delete'}>
+									<ImageField name={'image'} label={'Image'} />
+									<TextField name={'caption'} label={'Caption'} />
+								</SortableRepeater>
+							</ToOne>
+						</>,
+					],
+				]}
+			/>
 		</>
 	),
 	'GenericContentForm',
