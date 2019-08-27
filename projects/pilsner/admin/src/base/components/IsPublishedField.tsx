1121b5c18dc6298bc477d5cc0e6ed4e32024ff9c
Jirka Vebr <jirkavebr@gmail.com>
2019-08-27 18:56:21 +0200

    wip

--- projects/pilsner/admin/src/base/components/IsPublishedField.tsx
+++ projects/pilsner/admin/src/base/components/IsPublishedField.tsx
@@ -1,6 +1,38 @@
-import { Component, DateField } from 'cms-admin'
+import { Component, ConcealableField, DateFieldInner, Field, FormGroup, useRelativeSingleField } from 'cms-admin'
 import * as React from 'react'
 
-export const IsPublishedField = Component(() => {
-	return <DateField name="publishedAt" label="Publish date" />
-}, 'IsPublishedField')
+const fieldName = 'publishedAt'
+const now = new Date().toISOString()
+
+export const IsPublishedField = Component(
+	() => {
+		const dateField = useRelativeSingleField<string>(fieldName)
+
+		const renderConcealedValue = React.useCallback(() => {
+			if (dateField.currentValue === now || dateField.currentValue === null) {
+				return <i>now</i>
+			}
+			const date = new Date(dateField.currentValue)
+
+			return new Intl.DateTimeFormat('en-US', {
+				day: '2-digit',
+				month: '2-digit',
+				year: 'numeric',
+			}).format(date)
+		}, [dateField.currentValue])
+
+		return (
+			<FormGroup label="Publish date">
+				<ConcealableField renderConcealedValue={renderConcealedValue}>
+					{({ onFocus, onBlur }) => (
+						<Field<string> name={fieldName} defaultValue={now}>
+							{fieldMetadata => <DateFieldInner fieldMetadata={fieldMetadata} onFocus={onFocus} onBlur={onBlur} />}
+						</Field>
+					)}
+				</ConcealableField>
+			</FormGroup>
+		)
+	},
+	() => <Field name={fieldName} defaultValue={now} />,
+	'IsPublishedField',
+)
