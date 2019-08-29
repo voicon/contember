2896c5146daeb9ff84969d263c7ea83b228bd858
Jirka Vebr <jirkavebr@gmail.com>
2019-08-29 15:07:39 +0200

    Add a NavigateBackButton component

---
+++ projects/pilsner/admin/src/base/components/NavigateBackButton.tsx
@@ -0,0 +1,15 @@
+import { PageLinkButton, PageLinkButtonProps } from 'cms-admin'
+import * as React from 'react'
+
+export type NavigateBackButtonProps = PageLinkButtonProps
+
+export const NavigateBackButton = React.memo<NavigateBackButtonProps>(props => (
+	<PageLinkButton distinction="seamless" size="small" {...props}>
+		<span
+			style={{ fontSize: '.7em', display: 'inline-block', top: '-.09em', position: 'relative', marginRight: '.25em' }}
+		>
+			❰
+		</span>
+		{props.children}
+	</PageLinkButton>
+))
