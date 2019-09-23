a1edb76611b06b2930de9ec4778760bcc6cc8d3b
David Matejka <matej21@matej21.cz>
2019-09-23 09:52:28 +0200

    hranipex: add dim switcher

--- projects/hranipex/admin/src/prod/Layout.tsx
+++ projects/hranipex/admin/src/prod/Layout.tsx
@@ -1,9 +1,31 @@
-import { LayoutDefault } from 'cms-admin'
+import { DimensionsSwitcher, LayoutDefault, Literal } from 'cms-admin'
 import * as React from 'react'
 import logo from './logo.svg'
 import { SideMenu } from './SideMenu'
 
 interface LayoutProps {}
+
+const siteSwitcher = (
+	<DimensionsSwitcher
+		optionEntities="Site"
+		orderBy={[{ order: new Literal('asc') }]}
+		dimension="site"
+		maxItems={1}
+		labelField="code"
+		slugField="code"
+	/>
+)
+
+const localeSwitcher = (
+	<DimensionsSwitcher
+		optionEntities="SiteLocale"
+		filter="[site.code = $site]"
+		orderBy={[{ order: new Literal('asc') }]}
+		dimension="siteLocale"
+		labelField="code"
+		slugField="code"
+	/>
+)
 
 export class Layout extends React.PureComponent<LayoutProps> {
 	render() {
@@ -11,7 +33,16 @@ export class Layout extends React.PureComponent<LayoutProps> {
 			<LayoutDefault
 				header={{
 					title: logo,
-					left: false,
+					left: (
+						<div style={{ display: 'flex', alignItems: 'center' }}>
+							<div style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '3px 10px' }}>
+								Site: {siteSwitcher}
+							</div>
+							<div style={{ border: '1px solid #ccc', marginLeft: '10px', borderRadius: '5px', padding: '3px 10px' }}>
+								Locale: {localeSwitcher}
+							</div>
+						</div>
+					),
 					right: false,
 				}}
 				side={<SideMenu />}
