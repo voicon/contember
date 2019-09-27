aadba8298ed7ef3ab0be18052ac3f23a0cd5bac2
David Matejka <matej21@matej21.cz>
2019-09-27 12:53:56 +0200

    hrx: admin: add content editor to a post form

--- projects/hranipex/admin/src/prod/forms/PostForm.tsx
+++ projects/hranipex/admin/src/prod/forms/PostForm.tsx
@@ -1,7 +1,64 @@
-import { Box, Component, SlugField, TextAreaField, TextField } from 'cms-admin'
+import { Box, Component, RichEditorNG, SlugField, TextAreaField, TextField, ToOne } from 'cms-admin'
 import * as React from 'react'
-import { ImageField, IsPublishedField, LocaleSideDimension } from '../components'
+import { ImageField, IsPublishedField, LocaleSideDimension, SiteField } from '../components'
 import { SeoForm } from './'
+
+export const GenericContentForm = Component(
+	() => (
+		<ToOne field={'content'}>
+			<h2>Content</h2>
+			<SiteField />
+			<RichEditorNG
+				name="type"
+				sortBy={'order'}
+				field={'blocks'}
+				defaultBlock="text"
+				inlines={{
+					link: {
+						renderInline: ({ children, attributes, data, setData }) => (
+							<a
+								{...attributes}
+								style={{ textDecoration: 'underline', color: 'blue' }}
+								href={data.href}
+								onClick={e => {
+									e.preventDefault()
+									setData({ href: prompt('Enter url') })
+								}}
+							>
+								{children}
+							</a>
+						),
+						htmlSerializer: {},
+						keyboardShortcut: 'mod+l',
+						menuButton: () => 'L',
+					},
+				}}
+				marks={{
+					bold: {
+						renderMark: ({ children, attributes }) => <strong {...attributes}>{children}</strong>,
+						htmlSerializer: {},
+						keyboardShortcut: 'mod+b',
+						menuButton: () => 'B',
+					},
+				}}
+				blocks={{
+					text: {
+						label: 'Text',
+						renderBlock: ({ children }) => <p style={{ minHeight: '1em' }}>{children}</p>,
+						valueField: 'text',
+						marks: ['bold'],
+						inlines: ['link'],
+					},
+					image: {
+						label: 'Image',
+						render: <ImageField name={'image'} label={'Image'} />,
+					},
+				}}
+			/>
+		</ToOne>
+	),
+	'GenericContentForm',
+)
 
 export const PostForm = Component(
 	() => (
@@ -11,6 +68,7 @@ export const PostForm = Component(
 				<SlugField drivenBy="title" label={'Slug'} name={'slug'} />
 				<ImageField name={'image'} label={'Image'} />
 				<TextAreaField name="perex" label="Perex" />
+				<GenericContentForm />
 			</Box>
 			<SeoForm />
 		</LocaleSideDimension>
