9ad2c07dc94d1b924c4e41d114afc7e7f2ad688f
Filip Chalupa <chalupa.filip@gmail.com>
2019-09-30 10:09:11 +0200

    Pilsner: dashboard - box

--- projects/pilsner/admin/src/base/pages/DashboardPage.tsx
+++ projects/pilsner/admin/src/base/pages/DashboardPage.tsx
@@ -1,95 +1,111 @@
 import {
 	Button,
 	GenericPage,
+	Heading,
 	PageLink,
 	PageLinkButton,
+	Table,
+	TableCell,
+	TableRow,
 	Tile,
 	TileList,
 	TitleBar,
-	IncreaseHeadingDepth,
 } from 'cms-admin'
 import * as React from 'react'
 
 export const DashboardPage = (
 	<GenericPage pageName="dashboard">
 		<TitleBar>PilsnerUrquell admin</TitleBar>
-		<IncreaseHeadingDepth currentDepth={1}>
-			<div style={{ marginBottom: '1.5rem' }}>
-				<TileList>
-					<Tile heading="Production">
+		<div style={{ marginBottom: '1.5rem' }}>
+			<Table size="default">
+				<TableRow>
+					<TableCell>
+						<b>Production</b>
+					</TableCell>
+					<TableCell shrunk>
 						<a href="https://www.pilsnerurquell.com/" target="_blank" rel="noopener">
-							pilsnerurquell.com
+							www.pilsnerurquell.com
 						</a>
-					</Tile>
-					<Tile heading="Live preview">
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<b>Live preview</b>
+					</TableCell>
+					<TableCell shrunk>
 						<a href="https://live.pilsnerurquell.com/" target="_blank" rel="noopener">
 							live.pilsnerurquell.com
 						</a>
-					</Tile>
-					<Tile heading="Beta">
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<b>Beta</b>
+					</TableCell>
+					<TableCell shrunk>
 						<a href="https://beta.pilsner.mgw.cz/" target="_blank" rel="noopener">
 							beta.pilsner.mgw.cz
 						</a>
-					</Tile>
-				</TileList>
-			</div>
-			<TileList>
-				<Tile>
-					<img
-						src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-eye-4.png"
-						width={160}
-						height={160}
-						alt=""
-						style={{ opacity: 0.8 }}
-					/>
-					<br />
-					<br />
-					<PageLink
-						to="frontPage"
-						Component={({ isActive, ...buttonProps }) => (
-							<Button {...buttonProps} Component="a">
-								Edit Front page
-							</Button>
-						)}
-					/>
-				</Tile>
-				<Tile>
-					<img
-						src="https://cdns.iconmonstr.com/wp-content/assets/preview/2013/240/iconmonstr-note-19.png"
-						width={160}
-						height={160}
-						alt=""
-						style={{ opacity: 0.8 }}
-					/>
-					<br />
-					<br />
-					<PageLinkButton to="blogCreate">Compose a new blog post</PageLinkButton>
-				</Tile>
-				<Tile>
-					<img
-						src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-home-7.png"
-						width={160}
-						height={160}
-						alt=""
-						style={{ opacity: 0.8 }}
-					/>
-					<br />
-					<br />
-					<PageLinkButton to="pubCreate">Add a new pub</PageLinkButton>
-				</Tile>
-				<Tile>
-					<img
-						src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-user-6.png"
-						width={160}
-						height={160}
-						alt=""
-						style={{ opacity: 0.8 }}
-					/>
-					<br />
-					<br />
-					<PageLinkButton to="tapsterCreate">Add a new tapster</PageLinkButton>
-				</Tile>
-			</TileList>
-		</IncreaseHeadingDepth>
+					</TableCell>
+				</TableRow>
+			</Table>
+		</div>
+		<TileList>
+			<Tile>
+				<img
+					src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-eye-4.png"
+					width={160}
+					height={160}
+					alt=""
+					style={{ opacity: 0.8 }}
+				/>
+				<br />
+				<br />
+				<PageLink
+					to="frontPage"
+					Component={({ isActive, ...buttonProps }) => (
+						<Button {...buttonProps} Component="a">
+							Edit Front page
+						</Button>
+					)}
+				/>
+			</Tile>
+			<Tile>
+				<img
+					src="https://cdns.iconmonstr.com/wp-content/assets/preview/2013/240/iconmonstr-note-19.png"
+					width={160}
+					height={160}
+					alt=""
+					style={{ opacity: 0.8 }}
+				/>
+				<br />
+				<br />
+				<PageLinkButton to="blogCreate">Compose a new blog post</PageLinkButton>
+			</Tile>
+			<Tile>
+				<img
+					src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-home-7.png"
+					width={160}
+					height={160}
+					alt=""
+					style={{ opacity: 0.8 }}
+				/>
+				<br />
+				<br />
+				<PageLinkButton to="pubCreate">Add a new pub</PageLinkButton>
+			</Tile>
+			<Tile>
+				<img
+					src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-user-6.png"
+					width={160}
+					height={160}
+					alt=""
+					style={{ opacity: 0.8 }}
+				/>
+				<br />
+				<br />
+				<PageLinkButton to="tapsterCreate">Add a new tapster</PageLinkButton>
+			</Tile>
+		</TileList>
 	</GenericPage>
 )
