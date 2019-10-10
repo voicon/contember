16a871d1e816b0ccd95440b55d3f196fad68fa58
Jirka Vebr <jirkavebr@gmail.com>
2019-10-10 18:14:56 +0200

    Simpler & more general common renderer

--- projects/pilsner/admin/src/base/components/NavigateBackButton.tsx
+++
@@ -1,6 +0,0 @@
-import { PageLinkButton, PageLinkButtonProps } from 'cms-admin'
-import * as React from 'react'
-
-export type NavigateBackButtonProps = PageLinkButtonProps
-
-export { NavigateBackButton } from 'cms-admin'
