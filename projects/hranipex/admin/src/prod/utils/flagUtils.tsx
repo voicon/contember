cd1e35e0c4faacd2f70878b71496918fb100ccc4
Jirka Vebr <jirkavebr@gmail.com>
2019-10-15 14:32:29 +0200

    Hranipex: use correct flag images

--- projects/hranipex/admin/src/prod/utils/flagUtils.tsx
+++ projects/hranipex/admin/src/prod/utils/flagUtils.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
 
 export const getCountryFlag = (code: string) => (
-	<img src={'https://cz-hranipex.mgw.cz/assets/images/flags-country/' + code + '.svg'} style={{ height: '1em' }} />
+	<img
+		src={'https://s3.eu-central-1.amazonaws.com/hranipex.mgw.cz/assets/images/flags-country/' + code + '.svg'}
+		style={{ height: '1em' }}
+	/>
 )
 
 export const getLocaleFlag = (code: string) => (
-	<img src={'https://cz-hranipex.mgw.cz/assets/images/flags-locale/' + code + '.svg'} style={{ height: '1em' }} />
+	<img
+		src={'https://s3.eu-central-1.amazonaws.com/hranipex.mgw.cz/assets/images/flags-locale/' + code + '.svg'}
+		style={{ height: '1em' }}
+	/>
 )
