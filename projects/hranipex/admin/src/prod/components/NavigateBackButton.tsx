ab33ca21a9ac5da706dd05cd4bb0d5d3a59f7dc4
Filip Chalupa <chalupa.filip@gmail.com>
2019-10-22 13:53:43 +0200

    Icon: navigate back chevron

--- projects/hranipex/admin/src/prod/components/NavigateBackButton.tsx
+++ projects/hranipex/admin/src/prod/components/NavigateBackButton.tsx
@@ -1,15 +1,11 @@
-import { PageLinkButton, PageLinkButtonProps } from 'cms-admin'
+import { PageLinkButton, PageLinkButtonProps, Icon } from 'cms-admin'
 import * as React from 'react'
 
 export type NavigateBackButtonProps = PageLinkButtonProps
 
 export const NavigateBackButton = React.memo<NavigateBackButtonProps>(props => (
 	<PageLinkButton distinction="seamless" size="small" style={{ marginLeft: '-.3em' }} {...props}>
-		<span
-			style={{ fontSize: '.7em', display: 'inline-block', top: '-.09em', position: 'relative', marginRight: '.25em' }}
-		>
-			❰
-		</span>
+		<Icon blueprintIcon="chevron-left" />
 		{props.children}
 	</PageLinkButton>
 ))
