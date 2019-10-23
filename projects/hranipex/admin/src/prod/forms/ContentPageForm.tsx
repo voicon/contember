924a387cbc710e8df0ef29e95cf8d6c405d47c2d
Jirka Vebr <jirkavebr@gmail.com>
2019-10-23 14:00:36 +0200

    Hranipex: use sortable block repeater

--- projects/hranipex/admin/src/prod/forms/ContentPageForm.tsx
+++ projects/hranipex/admin/src/prod/forms/ContentPageForm.tsx
@@ -1,10 +1,13 @@
 import {
+	Block,
+	BlockType,
 	Box,
 	Component,
-	HiddenField,
-	Literal,
-	RichEditorNG,
+	LineBreakBehavior,
+	Mark,
+	RichTextField,
 	SlugField,
+	SortableBlockRepeater,
 	SortableRepeater,
 	TextField,
 	ToOne,
@@ -16,164 +19,90 @@ import { SeoForm } from './'
 export const ContentPageContentForm = Component(
 	() => (
 		<ToOne field={'content'}>
-			<h2>Content</h2>
 			<SiteField />
-			<RichEditorNG
-				name="type"
-				sortBy={'order'}
-				field={'blocks'}
-				defaultBlock="text"
-				inlines={{
-					link: {
-						renderInline: ({ children, attributes, data, setData }) => (
-							<a
-								{...attributes}
-								style={{ textDecoration: 'underline', color: 'blue' }}
-								href={data.href}
-								onClick={e => {
-									e.preventDefault()
-									setData({ href: prompt('Enter url') })
-								}}
-							>
-								{children}
-							</a>
-						),
-						htmlSerializer: {},
-						keyboardShortcut: 'mod+l',
-						menuButton: () => 'L',
-					},
-				}}
-				marks={{
-					bold: {
-						renderMark: ({ children, attributes }) => <strong {...attributes}>{children}</strong>,
-						htmlSerializer: {},
-						keyboardShortcut: 'mod+b',
-						menuButton: () => 'B',
-					},
-				}}
-				blocks={{
-					text: {
-						label: 'Text',
-						renderBlock: ({ children }) => <p style={{ minHeight: '1em' }}>{children}</p>,
-						valueField: 'text',
-						marks: ['bold'],
-						inlines: ['link'],
-					},
-					image: {
-						label: 'Image',
-						render: <ImageField name={'image'} label={'Image'} />,
-					},
-					textBox: {
-						label: <>Text box</>,
-						render: (
-							<>
-								<TextField name={'title'} size={'large'} label={'Title'} allowNewlines={true} />
-								<TextField name={'text'} label={'Text'} allowNewlines={true} />
-							</>
-						),
-					},
-					imageBoxLeft: {
-						label: <>Image box left</>,
-						render: (
-							<>
-								<TextField name={'title'} size={'large'} label={'Title'} allowNewlines={true} />
-								<TextField name={'text'} label={'Text'} allowNewlines={true} />
-								<ImageField name={'image'} label={'Image'} />
-							</>
-						),
-					},
-					imageBoxRight: {
-						label: <>Image box right</>,
-						render: (
-							<>
-								<TextField name={'title'} size={'large'} label={'Title'} allowNewlines={true} />
-								<TextField name={'text'} label={'Text'} allowNewlines={true} />
-								<ImageField name={'image'} label={'Image'} />
-							</>
-						),
-					},
-					imageBoxCircleLeft: {
-						label: <>Image box circle</>,
-						render: (
-							<>
-								<>
-									<TextField name={'title'} label={'Title'} allowNewlines={true} />
-									<TextField name={'text'} label={'Text'} allowNewlines={true} />
-									<ImageField name={'image'} label={'Image'} />
-								</>
-							</>
-						),
-					},
-					grid: {
-						label: <>Grid</>,
-						render: (
-							<>
-								<TextField name={'title'} label={'Title'} allowNewlines={true} />
-								<TextField name={'buttonCaption'} label={'Button caption'} />
-								<LinkField name={'buttonLink'} />
-								<ToOne field={'children'}>
-									<SortableRepeater field={'items'} sortBy={'order'} removeType={'delete'}>
-										<TextField name={'title'} label={'Title'} allowNewlines={true} />
-										<TextField name={'text'} label={'Text'} allowNewlines={true} />
-										<ImageField name={'image'} label={'Image'} />
-									</SortableRepeater>
-								</ToOne>
-							</>
-						),
-					},
-					circleList: {
-						label: <>Circle list</>,
-						render: (
-							<>
-								<TextField name={'title'} label={'Title'} allowNewlines={true} />
-								<TextField name={'subtitle'} label={'Subtitle'} allowNewlines={true} />
-								<TextField name={'text'} label={'Text'} allowNewlines={true} />
-								<ToOne field={'children'}>
-									<SortableRepeater field={'items'} sortBy={'order'} removeType={'delete'}>
-										<TextField name={'title'} label={'Title'} allowNewlines={true} />
-										<TextField name={'text'} label={'Text'} allowNewlines={true} />
-										<ImageField name={'image'} label={'Image'} />
-										<TextField name={'buttonCaption'} label={'Button caption'} />
-										{/*<LinkField name={'buttonLink'} />*/}
-									</SortableRepeater>
-								</ToOne>
-							</>
-						),
-					},
-					squareList: {
-						label: <>Square list</>,
-						render: (
-							<>
-								<TextField name={'title'} label={'Title'} allowNewlines={true} />
-								<TextField name={'subtitle'} label={'Subtitle'} allowNewlines={true} />
-								<TextField name={'text'} label={'Text'} allowNewlines={true} />
-								<ToOne field={'children'}>
-									<SortableRepeater field={'items'} sortBy={'order'} removeType={'delete'}>
-										<TextField name={'title'} label={'Title'} allowNewlines={true} />
-										<TextField name={'text'} label={'Text'} allowNewlines={true} />
-										<ImageField name={'image'} label={'Image'} />
-									</SortableRepeater>
-								</ToOne>
-							</>
-						),
-					},
-					timeline: {
-						label: <>Timeline</>,
-						render: (
-							<>
-								<TextField name={'title'} label={'Title'} allowNewlines={true} />
-								<TextField name={'text'} label={'Text'} allowNewlines={true} />
-								<ToOne field={'children'}>
-									<SortableRepeater field={'items'} sortBy={'order'} removeType={'delete'}>
-										<TextField name={'title'} label={'Title'} allowNewlines={true} />
-										<TextField name={'text'} label={'Text'} allowNewlines={true} />
-									</SortableRepeater>
-								</ToOne>
-							</>
-						),
-					},
-				}}
-			/>
+			<SortableBlockRepeater
+				sortBy="order"
+				field="blocks"
+				removeType="delete"
+				discriminationField="type"
+				label="Content"
+				enableUnlinkAll={true}
+			>
+				<Block discriminateBy="text" label="Text">
+					<RichTextField
+						label="Text"
+						name="text"
+						lineBreakBehavior={LineBreakBehavior.SMART}
+						blocks={[
+							{ block: BlockType.PARAGRAPH, marks: [Mark.BOLD, Mark.LINK, Mark.ITALIC, Mark.UNDERLINED] },
+							{ block: BlockType.HEADING_H2, marks: [] },
+							{ block: BlockType.HEADING_H3, marks: [] },
+						]}
+					/>
+				</Block>
+				<Block discriminateBy="image" label="Image">
+					<ImageField name={'image'} label={'Image'} />
+				</Block>
+				<Block discriminateBy="textBox" label="Text box">
+					<TextField name={'title'} size={'large'} label={'Title'} allowNewlines={true} />
+					<TextField name={'text'} label={'Text'} allowNewlines={true} />
+				</Block>
+				<Block discriminateBy="imageBoxLeft" label="Image box left">
+					<TextField name={'title'} size={'large'} label={'Title'} allowNewlines={true} />
+					<TextField name={'text'} label={'Text'} allowNewlines={true} />
+					<ImageField name={'image'} label={'Image'} />
+				</Block>
+				<Block discriminateBy="imageBoxRight" label="Image box right">
+					<TextField name={'title'} size={'large'} label={'Title'} allowNewlines={true} />
+					<TextField name={'text'} label={'Text'} allowNewlines={true} />
+					<ImageField name={'image'} label={'Image'} />
+				</Block>
+				<Block discriminateBy="imageBoxCircleLeft" label="Image box circle">
+					<TextField name={'title'} label={'Title'} allowNewlines={true} />
+					<TextField name={'text'} label={'Text'} allowNewlines={true} />
+					<ImageField name={'image'} label={'Image'} />
+				</Block>
+				<Block discriminateBy="grid" label="Grid">
+					<TextField name={'title'} label={'Title'} allowNewlines={true} />
+					<TextField name={'buttonCaption'} label={'Button caption'} />
+					<LinkField name={'buttonLink'} />
+					<SortableRepeater field={'children.items'} sortBy={'order'} removeType={'delete'}>
+						<TextField name={'title'} label={'Title'} allowNewlines={true} />
+						<TextField name={'text'} label={'Text'} allowNewlines={true} />
+						<ImageField name={'image'} label={'Image'} />
+					</SortableRepeater>
+				</Block>
+				<Block discriminateBy="circleList" label="Circle list">
+					<TextField name={'title'} label={'Title'} allowNewlines={true} />
+					<TextField name={'subtitle'} label={'Subtitle'} allowNewlines={true} />
+					<TextField name={'text'} label={'Text'} allowNewlines={true} />
+					<SortableRepeater field={'children.items'} sortBy={'order'} removeType={'delete'}>
+						<TextField name={'title'} label={'Title'} allowNewlines={true} />
+						<TextField name={'text'} label={'Text'} allowNewlines={true} />
+						<ImageField name={'image'} label={'Image'} />
+						<TextField name={'buttonCaption'} label={'Button caption'} />
+						{/*<LinkField name={'buttonLink'} />*/}
+					</SortableRepeater>
+				</Block>
+				<Block discriminateBy="squareList" label="Square list">
+					<TextField name={'title'} label={'Title'} allowNewlines={true} />
+					<TextField name={'subtitle'} label={'Subtitle'} allowNewlines={true} />
+					<TextField name={'text'} label={'Text'} allowNewlines={true} />
+					<SortableRepeater field={'children.items'} sortBy={'order'} removeType={'delete'}>
+						<TextField name={'title'} label={'Title'} allowNewlines={true} />
+						<TextField name={'text'} label={'Text'} allowNewlines={true} />
+						<ImageField name={'image'} label={'Image'} />
+					</SortableRepeater>
+				</Block>
+				<Block discriminateBy="timeline" label="Timeline">
+					<TextField name={'title'} label={'Title'} allowNewlines={true} />
+					<TextField name={'text'} label={'Text'} allowNewlines={true} />
+					<SortableRepeater field={'children.items'} sortBy={'order'} removeType={'delete'}>
+						<TextField name={'title'} label={'Title'} allowNewlines={true} />
+						<TextField name={'text'} label={'Text'} allowNewlines={true} />
+					</SortableRepeater>
+				</Block>
+			</SortableBlockRepeater>
 		</ToOne>
 	),
 	'GenericContentForm',
